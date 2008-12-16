d2ba7250b5d86b34e436565c63aefa09b997b04e
Scott Taylor <scott@railsnewbie.com>
2008-12-16 03:25:21 -0500

    * Dont' create begin..end blocks when it's not necessary to do so

--- lib/treetop/compiler/node_classes/terminal.rb
+++ lib/treetop/compiler/node_classes/terminal.rb
@@ -5,16 +5,14 @@ module Treetop
         super
         string_length = eval(text_value).length
 
-        builder.begin_end do
-          builder.if__ "input.index(#{text_value}, index) == index" do
-            assign_result "(#{node_class_name}).new(input, index...(index + #{string_length}))"
-            extend_result_with_inline_module
-            builder << "@index += #{string_length}"
-          end
-          builder.else_ do
-            builder << "terminal_parse_failure(#{text_value})"
-            assign_result 'nil'
-          end
+        builder.if__ "input.index(#{text_value}, index) == index" do
+          assign_result "(#{node_class_name}).new(input, index...(index + #{string_length}))"
+          extend_result_with_inline_module
+          builder << "@index += #{string_length}"
+        end
+        builder.else_ do
+          builder << "terminal_parse_failure(#{text_value})"
+          assign_result 'nil'
         end
       end
     end
