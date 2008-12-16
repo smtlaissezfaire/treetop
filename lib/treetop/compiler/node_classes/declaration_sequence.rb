465c0c9a96202938da07526d35152efc5d061ea7
Scott Taylor <scott@railsnewbie.com>
2008-12-15 23:51:40 -0500

    * Refactor DeclarationSequence#compile

--- lib/treetop/compiler/node_classes/declaration_sequence.rb
+++ lib/treetop/compiler/node_classes/declaration_sequence.rb
@@ -3,21 +3,28 @@ module Treetop
     class DeclarationSequence < Runtime::SyntaxNode
 
       def compile(builder)
-        unless rules.empty?
-          builder.method_declaration("root") do
-            builder << "@root || :#{rules.first.name}"
-          end
-          builder.newline
-        end
-        
-        declarations.each do |declaration|
-          declaration.compile(builder)
-          builder.newline
-        end
+        define_root_method(builder) unless rules.empty?
+        define_each_declaration(builder)
       end
       
       def rules
         declarations.select { |declaration| declaration.instance_of?(ParsingRule) }
+      end
+
+    private
+
+      def define_root_method(builder)
+        builder.method_declaration("root") do
+          builder << "@root || :#{rules.first.name}"
+        end
+        builder.newline
+      end
+
+      def define_each_declaration(builder)
+        declarations.each do |declaration|
+          declaration.compile(builder)
+          builder.newline
+        end
       end
     end
   end
